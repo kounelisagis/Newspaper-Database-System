9663cbf5dc988678faffa6791e634487cbd7bc23
Agisilaos Kounelis <36283973+kounelisagis@users.noreply.github.com>
2020-02-08 03:00:40 +0200

    v3

--- insert.sql
+++ insert.sql
@@ -1,79 +1,118 @@
 USE db1;
 
+
 /* INSERTS */
-INSERT INTO publisher VALUES
-  ('mymail@upatras.gr', 'Ilias', 'Fountoulis'),
-  ('anothermail@upatras.gr', 'Kwstas', 'Mpalaroutsos');
+INSERT INTO publisher
+	(email, name, surname)
+VALUES
+	('mymail@upatras.gr', 'Ilias', 'Fountoulis'),
+	('anothermail@upatras.gr', 'Kwstas', 'Mpalaroutsos');
 
 
-INSERT INTO worker VALUES
-  ('st1059637@upatras.gr', 'Agisilaos', 'Kounelis', 99999, '1999-11-01'),
-  ('st1059636@upatras.gr', 'Michael', 'Karavokyris', 5, '2005-06-30'),
-  ('st1059638@upatras.gr', 'Giannis', 'Koutsocheras', 500, '2015-06-30'),
-  ('st1059629@upatras.gr', 'Makis', 'Papachronopoulos', 50, '2018-06-30');
+INSERT INTO worker
+	(email, name, surname, salary, recruitment_date)
+VALUES
+	('st1059637@upatras.gr', 'Agisilaos', 'Kounelis', 9999, '1999-11-01'),
+	('st1059636@upatras.gr', 'Michael', 'Karavokyris', 5, '2005-06-30'),
+	('st1059638@upatras.gr', 'Giannis', 'Koutsocheras', 500, '2015-06-30'),
+	('st1059629@upatras.gr', 'Makis', 'Papachronopoulos', 50, '2018-06-30');
 
 
-INSERT INTO reporter VALUES 
-  ('st1059637@upatras.gr', 10, 'nasa'),
-  ('st1059636@upatras.gr', 1, 'makisAE'),
-  ('st1059638@upatras.gr', 2, 'fbi');
+INSERT INTO reporter
+	(email, work_experience, cv)
+VALUES
+	('st1059637@upatras.gr', 10, 'nasa'),
+	('st1059636@upatras.gr', 1, 'makisAE'),
+	('st1059638@upatras.gr', 2, 'fbi');
 
 
-INSERT INTO newspaper VALUES
-  ('Kathimerini', 'Mitsos Papazoglou', 'daily', 'mymail@upatras.gr', 'st1059637@upatras.gr'),
-  ('Katheminiea', 'George Belisariou', 'monthly', 'anothermail@upatras.gr', 'st1059636@upatras.gr');
+INSERT INTO newspaper
+	(name, owner, frequency, publisher, chief_editor)
+VALUES
+	('Kathimerini', 'Mitsos Papazoglou', 'daily', 'mymail@upatras.gr', 'st1059637@upatras.gr'),
+	('Katheminiea', 'George Belisariou', 'monthly', 'anothermail@upatras.gr', 'st1059636@upatras.gr');
 
 
-INSERT INTO page VALUES 
-  (1, '2020-4-1', 5, 10000, 'Kathimerini'),
-  (2, '2020-4-1', 7, 99999, 'Katheminiea');
+INSERT INTO paper
+	(publish_date, pages, copies, newspaper)
+VALUES
+	('2020-4-1', 6, 10000, 'Kathimerini'),
+	('2020-4-1', 7, 99999, 'Katheminiea');
 
 
-INSERT INTO category VALUES
-  (1, 'Athlitika', 'Ch League, Superleague', NULL),
-  (2, 'Enhmerwshs', 'Epikairothta TWRA', NULL);
-  
-
-INSERT INTO article VALUES 
-  ('C:\Users\PC\article1.doc', 'Article1', 1, 'Olympiakos', 'INITIAL','2020-3-1', 1, 1, 3),
-  ('C:\Users\PC\article2.doc', 'Article2', 2, 'Tsipras', 'ACCEPTED','2020-3-1', 1, 2, 2);
-
-INSERT INTO key_words VALUES
-  ('C:\Users\PC\article1.doc', 'key'),
-  ('C:\Users\PC\article2.doc', 'word');
-
-  
-INSERT INTO administrator VALUES
-  ('st1059629@upatras.gr', 'ton paizei', 'plaz', 69, 'patras');
+INSERT INTO category
+	(id, name, description, my_category)
+VALUES
+	(1, 'Athlitika', 'Ch League, Superleague', NULL),
+	(2, 'Enhmerwshs', 'Epikairothta TWRA', NULL);
 
 
-INSERT INTO phone_numbers VALUES
-  ('st1059629@upatras.gr', 6988567445);
+INSERT INTO article
+	(path, paper, title, description, category, num_of_pages, newspaper)
+VALUES
+	('C:\Users\PC\article1.doc', 1, 'Article1', 'Olympiakos', 1, 3.1, 'Kathimerini'),
+	('C:\Users\PC\article2.doc', 1, 'Article2', 'Tsipras', 2, 1.9, 'Kathimerini');
 
 
-INSERT INTO works VALUES
-  ('Kathimerini', 'st1059636@upatras.gr'),
-  ('Kathimerini', 'st1059637@upatras.gr'),
-  ('Katheminiea', 'st1059638@upatras.gr'),
-  ('Katheminiea', 'st1059629@upatras.gr');
+INSERT INTO key_words
+	(article, key_word)
+VALUES
+	('C:\Users\PC\article1.doc', 'key'),
+	('C:\Users\PC\article2.doc', 'word');
 
 
-INSERT INTO submission VALUES
-  ('st1059637@upatras.gr', 'C:\Users\PC\article1.doc', '2019-06-12'),
-  ('st1059636@upatras.gr', 'C:\Users\PC\article2.doc', '2018-03-20');
+INSERT INTO administrator
+	(email, duty, street, number, city)
+VALUES
+	('st1059629@upatras.gr', 'ton paizei', 'plaz', 69, 'patras');
+
+
+INSERT INTO phone_numbers
+	(admin, number)
+VALUES
+	('st1059629@upatras.gr', 6988567445);
+
+
+INSERT INTO works
+	(newspaper, worker)
+VALUES
+	('Kathimerini', 'st1059636@upatras.gr'),
+	('Kathimerini', 'st1059637@upatras.gr'),
+	('Katheminiea', 'st1059638@upatras.gr'),
+	('Katheminiea', 'st1059629@upatras.gr');
+
+
+INSERT INTO submission
+	(reporter, article, date)
+VALUES
+	('st1059637@upatras.gr', 'C:\Users\PC\article1.doc', '2019-06-12'),
+	('st1059636@upatras.gr', 'C:\Users\PC\article2.doc', '2018-03-20');
+
 
 /*
-afterakia
+afterchecks
 
-INSERT INTO article VALUES 
-  ('C:\Users\PC\article3333433.doc', 'Article13434343', 3, 'Olympiakos', 'INITIAL','2020-02-01', 1, 1, 1.4);
+INSERT INTO article
+	(path, paper, title, description, category, num_of_pages, newspaper)
+VALUES
+	('C:\Users\PC\article3333433.doc', 1, 'Article13434343', 'Olympiakos', 1, 0.9 ,'Kathimerini');
 
-INSERT INTO submission VALUES
-  ('st1059638@upatras.gr', 'C:\Users\PC\article3333433.doc', '2020-02-02');
 
-INSERT INTO article VALUES 
-  ('C:\Users\PC\oti8elw8akanw.doc', 'oti8elw', 4, 'Olympiakos', 'INITIAL','2020-02-01', 1, 1, 0.9);
+INSERT INTO submission
+	(reporter, article, date)
+VALUES
+	('st1059638@upatras.gr', 'C:\Users\PC\article3333433.doc', '2020-02-02');
 
-INSERT INTO submission VALUES
-  ('st1059637@upatras.gr', 'C:\Users\PC\oti8elw8akanw.doc', '2020-02-02');
+
+INSERT INTO article
+	(path, paper, title, description, category, num_of_pages, newspaper)
+VALUES
+	('C:\Users\PC\oti8elw8akanw.doc', 1, 'My_article', 'Olympiakos', 1, 1.4, 'Kathimerini');
+
+
+INSERT INTO submission
+	(reporter, article, date)
+VALUES
+	('st1059637@upatras.gr', 'C:\Users\PC\oti8elw8akanw.doc', '2020-02-02');
+
 */
--- stores.sql
+++ stores.sql
@@ -1,12 +1,14 @@
 USE db1;
 
-DROP PROCEDURE IF EXISTS paperSummary;
+
+DROP PROCEDURE IF EXISTS PaperSummary;
+
 DELIMITER $
-CREATE PROCEDURE paperSummary(IN pageid INT, IN newspapername VARCHAR(128))
+CREATE PROCEDURE PaperSummary(IN paper_id INT, IN newspaper VARCHAR(128))
 BEGIN
 	DECLARE finishedFlag INT;
-    DECLARE current_path VARCHAR(128);
-    DECLARE sum FLOAT DEFAULT 1.0;
+	DECLARE current_path VARCHAR(128);
+	DECLARE sum FLOAT DEFAULT 1.0;
 	DECLARE pages_available FLOAT;
 	DECLARE current_title VARCHAR(128);
 	DECLARE checkdate DATE;
@@ -14,68 +16,64 @@ BEGIN
 
 
 	/* Each select fetches path, title, pages_needed */
-    DECLARE bcursor CURSOR FOR
-	SELECT article.path, article.title, article.num_of_pages, article.checkdate FROM article
+	DECLARE bcursor CURSOR FOR
+	SELECT article.path, article.title, article.num_of_paper, article.checkdate FROM article
 	WHERE article.state = 'ACCEPTED' AND article.path in (
 		SELECT submission.article FROM works, submission
-		WHERE works.worker = submission.reporter AND works.newspaper = newspapername
+		WHERE works.worker = submission.reporter AND works.newspaper = newspaper
 	)
-    ORDER BY article.num_in_page ASC;
+	ORDER BY article.order_in_paper ASC;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
+	DECLARE CONTINUE HANDLER FOR NOT FOUND
 	SET finishedFlag = 1;
 
 	SET finishedFlag = 0;
-
-    OPEN bcursor;
-    REPEAT
+	OPEN bcursor;
+	REPEAT
 		FETCH bcursor INTO current_path, current_title, pages_needed, checkdate;
-        IF(finishedFlag = 0)
-        THEN
+		IF(finishedFlag = 0)
+		THEN
 
 			SELECT current_title as title, worker.name, worker.surname, checkdate, FLOOR(sum) as start_page, pages_needed
 			FROM worker, submission
 			WHERE worker.email = submission.reporter AND submission.article = current_path;
 
 			SET sum = sum + pages_needed;
-        END IF;
-    UNTIL(finishedFlag = 1)   
+		END IF;
+	UNTIL(finishedFlag = 1)   
 	END REPEAT;
-    CLOSE bcursor;
+	CLOSE bcursor;
 
-	SELECT page.pages INTO pages_available FROM page WHERE page.id = pageid;
+	SELECT paper.pages INTO pages_available FROM paper WHERE paper.id = paper_id;
 
-    IF (pages_available > sum) THEN  
+	IF (pages_available > sum) THEN  
 		SET pages_available = pages_available - sum + 1;
 		SELECT pages_available;
-    END IF;
-
+	END IF;
 END$
 DELIMITER ;
-
 
 
 /*--------------------*/
 
 
+DROP PROCEDURE IF EXISTS CalculateSalary;
 
-DROP PROCEDURE IF EXISTS calculate_salary;
 DELIMITER $
-CREATE PROCEDURE calculateSalary(IN reporter_email VARCHAR(30), OUT new_salary FLOAT)
+CREATE PROCEDURE CalculateSalary(IN reporter_email VARCHAR(30), OUT new_salary FLOAT)
 BEGIN
 	DECLARE reporter_workexperience INT;
-	DECLARE worker_startdate DATE;
-    DECLARE months_dif FLOAT;
-    DECLARE total_months INT;
+	DECLARE worker_recruitment_date DATE;
+	DECLARE months_dif FLOAT;
+	DECLARE total_months INT;
 	
-	SELECT reporter.work_before_startdate INTO reporter_workexperience FROM reporter WHERE reporter.email = reporter_email;
-	SELECT worker.startdate INTO worker_startdate FROM worker WHERE worker.email = reporter_email;
+	SELECT reporter.work_experience INTO reporter_workexperience FROM reporter WHERE reporter.email = reporter_email;
+	SELECT worker.recruitment_date INTO worker_recruitment_date FROM worker WHERE worker.email = reporter_email;
 
-    SET months_dif = FLOOR(DATEDIFF(CURDATE(), worker_startdate)/30.0);
-    SET total_months = months_dif + reporter_workexperience;
+	SET months_dif = FLOOR(DATEDIFF(CURDATE(), worker_recruitment_date)/30.0);
+	SET total_months = months_dif + reporter_workexperience;
 
 	SET new_salary = 650 + (total_months * 0.05) * 650;
-
 END$
 DELIMITER ;
 
@@ -83,9 +81,10 @@ DELIMITER ;
 /*--------------------*/
 
 
-DROP TRIGGER IF EXISTS setSalary;
+DROP TRIGGER IF EXISTS SetSalary;
+
 DELIMITER $
-CREATE TRIGGER setSalary BEFORE INSERT ON worker
+CREATE TRIGGER SetSalary BEFORE INSERT ON worker
 FOR EACH ROW
 BEGIN
 	SET NEW.salary = 650;
@@ -96,10 +95,10 @@ DELIMITER ;
 /*--------------------*/
 
 
-DROP TRIGGER IF EXISTS checkForChief;
+DROP TRIGGER IF EXISTS CheckForChief;
 
 DELIMITER $
-CREATE TRIGGER checkForChief BEFORE INSERT ON submission
+CREATE TRIGGER CheckForChief BEFORE INSERT ON submission
 FOR EACH ROW
 BEGIN	
 	IF EXISTS (SELECT * FROM newspaper WHERE chief_editor = NEW.reporter) THEN
@@ -107,26 +106,33 @@ BEGIN
 		SET state = 'ACCEPTED'
 		WHERE path = NEW.article;
 	END IF;
-
 END$
 DELIMITER ;
 
 
+/*--------------------*/
 
-DROP TRIGGER IF EXISTS checkForPages;
+
+DROP TRIGGER IF EXISTS CheckForPages;
 
 DELIMITER $
-CREATE TRIGGER checkForPages BEFORE INSERT ON article
+CREATE TRIGGER CheckForPages BEFORE INSERT ON article
 FOR EACH ROW
-BEGIN	
-	/*IF EXISTS (SELECT * FROM newspaper WHERE chief_editor = NEW.reporter) THEN
-		UPDATE article
-		SET state = 'ACCEPTED'
-		WHERE path = NEW.article;
-	END IF;*/
-	DECLARE @returnvalue INT
-	EXEC @returnvalue = SP_One
+BEGIN
+	DECLARE available_pages INT;
+	DECLARE written_pages FLOAT;
 
+	SELECT paper.pages INTO available_pages
+	FROM paper
+	WHERE NEW.paper = paper.id AND NEW.newspaper = paper.newspaper;
+
+	SELECT SUM(article.num_of_pages) INTO written_pages
+	FROM article
+	WHERE NEW.paper = article.paper AND NEW.newspaper = article.newspaper;
+
+	IF (written_pages + NEW.num_of_pages > available_pages) THEN
+		SIGNAL SQLSTATE VALUE '45000'
+		SET MESSAGE_TEXT = 'No space available';
+	END IF;
 END$
 DELIMITER ;
-
